66eff0950d482571ad6c88dc70c34bef39e6b26a
matanki.saito <matanki.saito@gmail.com>
2022-09-14 18:52:50 +0900

    Bump up 1.33.0 to 1.34.2

--- main.py
+++ main.py
@@ -229,7 +229,7 @@ def main():
         mod_title_name="JPMOD Sub 1: Characters, Cultures",
         mod_tags={"Translation", "Localisation"},
         mod_image_file_path="title.jpg",
-        mod_supported_version="1.33.*.*")
+        mod_supported_version="1.34.*.*")
 
     print("mod_pack_file_path:{}".format(mod_pack_file_path))
 
